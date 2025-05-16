87d8b46e6b6cae56100ecb84b8f8b4d2c954d624
Gustavo <gustaoliver18@gmail.com>
2025-05-15 22:43:21 -0300

    Configuração do banco de dados por consts

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-
+require_once 'config'. DIRECTORY_SEPARATOR. 'config.php';
 require_once "Lib/Application.php";
 //Os controllers conseguem ter acesso ao Application por  que ele está sendo instanciado no index
 $a = new Application();
